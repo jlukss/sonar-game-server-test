96e5fbdbd6f6f0aa50c3fc162a4bf90d176fb3fc
jlukss <jlukss@gmail.com>
2023-03-05 13:29:02 +0200

    Gunzip messages

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const dgram = require('dgram');
+const zlib = require('node:zlib');
 const server = dgram.createSocket('udp4');
 // const matches = require('./matches.js');
 
 const subscribers = [];
 
 server.on('message', (message, rinfo) => {
-  // console.log(`Received message: ${message} from ${rinfo.address}:${rinfo.port}`);
 
   if (message.toString().startsWith('CONNECT')) {
     // playerId = message.toString().substring(7);
     // match = matches.getMatchByPlayer(playerId);
     
     addSubscriber(rinfo.address, rinfo.port);
-  } else {
-    // Broadcast the message to all subscribers except the sender
-    subscribers.forEach((subscriber) => {
-      //if (subscriber.address !== rinfo.address || subscriber.port !== rinfo.port) {
-        server.send(message, subscriber.port, subscriber.address);
-        console.log(`Sent message: ${message} to ${subscriber.address}:${subscriber.port}`);
-    //  }
+  } else {  
+    zlib.gunzip(message, (err, uncompressedMsg) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      console.log(`Received message: ${uncompressedMsg} from ${rinfo.address}:${rinfo.port}`);
+      // Broadcast the message to all subscribers except the sender
+      subscribers.forEach((subscriber) => {
+        //if (subscriber.address !== rinfo.address || subscriber.port !== rinfo.port) {
+          server.send(message, subscriber.port, subscriber.address);
+          console.log(`Sent message: ${message} to ${subscriber.address}:${subscriber.port}`);
+      //  }
+      });
     });
   }
 });
